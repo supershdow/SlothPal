37e572c9f93c454ecbc6429f75435ea69175d001
supershdow <benjihofing@gmail.com>
2015-10-24 14:23:21 -0400

    Basics

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def root():
-    return 'Hi'
+    return render_template('base.html')
 
 
 if __name__=='__main__':
