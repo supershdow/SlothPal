fe9042aeeb502a46602ed08910dd5473036793a9
supershdow <benjihofing@gmail.com>
2015-10-24 17:00:34 -0400

    Login

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from flask import Flask,render_template,request,session,redirect
-from util import login
+from util import login as log
 
 app=Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('base.html')
+    return render_template('login-form.html')
 
-@app.route('/login',methods=['POST'])
-def login():
+@app.route('/login',methods=['GET','POST'])
+def log_in():
+    if request.method=="GET":
+        return 'GET'
+    elif request.method=="POST":
+        data=request.form
+        if log.validate(data['username'],data['password'])==True:
+            #session['username']=data['username']
+            return redirect('/home')
+        else:
+            return 'Invalid login'
+    else:
+        return 'yo'
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
     data=request.form
-    if validate(data['username'],data['password']):
-        session['username']=data['username']
-        return redirect('/home')
+    if request.method=='GET':
+        return render_template('signup.html')
+    elif request.method=='POST':
+        log.signup(data['username'],data['password'])
+        return 'Successfully signed up'
+    else:
+        return 'yo'
 
 @app.route('/home')
 def home():
@@ -21,10 +39,10 @@ def home():
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect('/')
+    #session.pop('username', None)
+    return 'logout'
 
 if __name__=='__main__':
-    app.secret_key='\xf2\xd4\x9c\xc8\xf6~\xabk|\x8bL\xfbfK\x7f\xc5\xc3\xc4\x0bX\xa7\xf3\x91\x1f'
+    #app.secret_key='\xf2\xd4\x9c\xc8\xf6~\xabk|\x8bL\xfbfK\x7f\xc5\xc3\xc4\x0bX\xa7\xf3\x91\x1f'
     app.debug=True
     app.run()
--- util/login.py
+++ util/login.py
@@ -3,21 +3,21 @@ import Reader as read
 def validate(username, password):
     if username=='' or password=='':
         return False
-    creds=read.getCsvDict('credentials.txt')
-    if creds[username]==password:
+    creds=read.getCsvDict('./util/credentials.txt')
+    if username in creds.keys() and creds[username][0]==password:
         return True
     else:
         return False
 
 def signup(new_username,new_password):
-    previousCredentials=read.read_file('credentials.txt')
+    previousCredentials=read.read_file('./util/credentials.txt')
     new_credentials='%s,%s'%(new_username,new_password)
     if new_username=='' or new_password=='' or new_username in previousCredentials or new_username.find(',')!=-1 or new_password.find(',')!=-1:
         return 'Invalid signup credentials'
     else:
-        read.write_file('credentials.txt',new_credentials,'a')
+        read.write_file('./util/credentials.txt',new_credentials+'\n','a')
         return 'Successfully signed up'
 
 if __name__=='__main__':
-    validate('magic','unicorns')
-    signup('Sloth','themed')
+    print validate('magic','unicorns')
+    print signup('sloth','themed')
